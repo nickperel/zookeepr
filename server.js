abf3a5f4a987f51dab6ef62d3d62b7396c55f3ef
Kakashi <nickperel@live.com>
2022-01-09 22:42:53 -0500

    set up the new front end, created routes to serve index.html, finished POST functionality, created routes to serve other pages, finished GET requests to serve page data

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -113,4 +115,20 @@ app.post('/api/animals', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
